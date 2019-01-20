af44125db64c3adc9e068bf2430bb68a346ea8b5
PeterKudin <petf3@mail.ru>
2019-01-20 16:09:57 +0300

    Первая версия

---
+++ src/main/java/TestBot.java
@@ -0,0 +1,57 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+
+public class TestBot extends TelegramLongPollingBot {
+    private static final String BOT_NAME = "Misha2010TestBot";
+    private static final String BOT_TOKEN = "722649547:AAFobeolFSuWDdTU3PBiRqEDdJVkF_Wnc40";
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botapi = new TelegramBotsApi();
+        try {
+            botapi.registerBot(new TestBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            if (message.getText().equals("/help"))
+                sendMsg(message, "Привет, я робот");
+            else
+                sendMsg(message, "Я не знаю что ответить на это");
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
