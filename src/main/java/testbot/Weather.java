54f5d870262da560df0ec22f3775cddd545b4750
PeterKudin <petf3@mail.ru>
2019-02-10 16:18:34 +0300

    Поменяли погоду

--- src/main/java/testbot/Weather.java
+++ src/main/java/testbot/Weather.java
@@ -2,7 +2,8 @@ package testbot;
 
 public class Weather {
     public String getWeather(String city) {
-        return  "Сегодня в городе " +  city + " -4 градуса. " ;
+        return  "Сегодня в городе " +  city + " -4 градуса. " +
+                "Завтра утром -4...-2, днем -2..0" ;
 
     }
     public String getSeason(String city) {
