b07c78083481237a4c839c741aef31ea7340437f
PeterKudin <petf3@mail.ru>
2019-02-24 17:19:40 +0300

    Урок 4: Добавление в клавиатуру 5-й и 6-й строк

--- src/main/java/testbot/BotCommandLesson4.java
+++ src/main/java/testbot/BotCommandLesson4.java
@@ -6,14 +6,12 @@ public class BotCommandLesson4 extends BotCommand {
 
         if (text.equals("урок 4") || (text.equals("привет"))) {
             answer.text = "Привет! Да здравствуют роботы!!!";
-            answer.Row1.add("1.1");
-            answer.Row2.add("2.2");
-            answer.Row3.add("3.3");
-            answer.Row4.add("4.4");
-            answer.Row5.add("5.5");
-            answer.Row6.add("6.6");
+            answer.Row1.add("1+1");
+            answer.Row2.add("2+2");
+            answer.Row3.add("3+3");
+            answer.Row4.add("4+4");
+            answer.Row5.add("5+5");
+            answer.Row6.add("6+6");
         }
-        if (text.equals("1.1"))
-            answer.text = "Строка 1, столбец 1";
     }
 }
