b04125b649960c514a1868fba1fec7d7f0cfda86
PeterKudin <petf3@mail.ru>
2019-03-09 11:42:57 +0300

    Научили бота отвечать на приветствие Доброе утро.

--- src/main/java/testbot/BotCommandGoodMorning.java
+++ src/main/java/testbot/BotCommandGoodMorning.java
@@ -6,21 +6,30 @@ import java.util.TimeZone;
 public class BotCommandGoodMorning extends BotCommand {
 
     public void process(String text, Answer answer) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeZone(TimeZone.getTimeZone("Europe/Moscow"));
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        boolean isMorning =(hour >= 5) && (hour <= 11);
+        boolean isDea = ((hour >= 12) && (hour <= 15));
+
+
+        if (text.equals("доброе утро")) {
+            if (isMorning)  answer.text = "Доброе утро";
+            if (isDea)  answer.text = "Ну какой же сейчас день, сейчас ведь утро. Доброе утро";
+
+            }
         if (text.equals("привет")) {
             answer.withname = true;
 
-            Calendar cal = Calendar.getInstance();
-            cal.setTimeZone(TimeZone.getTimeZone("Europe/Moscow"));
-            int hour = cal.get(Calendar.HOUR_OF_DAY);
 
-            if ((hour >= 5) && (hour <= 11)) {
-                answer.text = "Доброе утро!!!";
+            if (isMorning)  {
+                answer.text = "Доброе утро!";
             }
             if ((hour >= 12) && (hour <= 15)) {
-                answer.text = "Добрый день!!!";
+                answer.text = "Добрый день!";
             }
             if ((hour >= 16) && (hour <= 23)) {
-                answer.text = "Добрый вечер!!!";
+                answer.text = "Добрый вечер!";
             }
             answer.text = "Сейчас " + hour + " часов" + answer.text;
         }
