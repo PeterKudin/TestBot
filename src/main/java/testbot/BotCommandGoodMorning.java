37326a4cf9931513d4a54b070672626b72b85ffd
PeterKudin <petf3@mail.ru>
2019-03-10 16:05:26 +0300

    Добавили команду contains.

--- src/main/java/testbot/BotCommandGoodMorning.java
+++ src/main/java/testbot/BotCommandGoodMorning.java
@@ -13,16 +13,23 @@ public class BotCommandGoodMorning extends BotCommand {
         boolean isDay = ((hour >= 12) && (hour <= 15));
         boolean isEvening=(hour >= 16) && (hour <= 23);
 
-        if (text.equals("доброе утро")) {
+        if (text.contains("доброе")&&(text.contains("утро"))) {
             if (isMorning) answer.text = "Доброе утро";
             if (isDay) answer.text = "Ну какое же сейчас утро, сейчас ведь день. Добрый день";
             if (isEvening) answer.text = "Ну какое же сейчас утро, сейчас ведь вечер. Добрый вечер";
         }
-        if (text.equals("добрый день")) {
+
+        if (text.contains("добрый")&&(text.contains("день"))) {
             if (isMorning) answer.text = "Ну какой же сейчас день, сейчас ведь утро. Доброе утро";
             if (isDay) answer.text = "Добрый день";
             if (isEvening) answer.text = "Ну какой же сейчас день, сейчас ведь вечер. Добрый вечер";
         }
+        if (text.contains("добрый")&&(text.contains("вечер"))) {
+            if (isMorning) answer.text = "Ну какой же сейчас вечер, сейчас ведь утро. Доброе утро";
+            if (isDay) answer.text = "Ну какой же сейчас вечер, сейчас ведь день. Добрый день";
+            if (isEvening) answer.text = "Добрый вечер";
+        }
+
         if (text.equals("привет")) {
             answer.withname = true;
 
