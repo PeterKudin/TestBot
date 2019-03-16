04590ee190636fa2eb1597bd4338375e0294c3ea
PeterKudin <petf3@mail.ru>
2019-03-16 14:28:48 +0300

    Учим отвечать какой сегодня день недели.

--- src/main/java/testbot/BotCommandLesson9.java
+++ src/main/java/testbot/BotCommandLesson9.java
@@ -2,21 +2,19 @@
 Задание, урок 9: учим бота сообщать какой сегодня день недели
 1. Бот должен уметь отвечать на фразу 'какой сегодня день недели', со всевозможными перестановками слов (например: сегодня какой день недели, день недели сегодня какой, какой день недели сегодня)
 2. Для получения текущего дня недели нужно вписать следующий код (в переменной dayOfWeek будет название дня недели):
-  Format dateFormat = new SimpleDateFormat("EEE");
-  String dayOfWeek = dateFormat.format(new Date());
+  String dayOfWeek = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(new Date());
 
 */
 package testbot;
 
-import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class BotCommandLesson9 extends BotCommand {
 
     public void process(String text, Answer answer) {
-        Format dateFormat = new SimpleDateFormat("EEE");
-        String dayOfWeek = dateFormat.format(new Date());
+        String dayOfWeek = new SimpleDateFormat("EEEE", new Locale("ru")).format(new Date());
 
         if (text.equals("урок 9")) {
             answer.text = "Сегодня " + dayOfWeek;
